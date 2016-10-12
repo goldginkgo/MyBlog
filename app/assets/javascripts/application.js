3879bfdbe24fde1a4fadac6a0c7f524b6ee7db9a
Frank Dai <yongjindai09@gmail.com>
2016-10-12 03:11:37 +0000

    Add tinymce plugins

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,5 +18,9 @@
 
 document.addEventListener("turbolinks:load", function() {
   tinymce.remove();
-  tinymce.init({ selector:'textarea#post_body' });
+  tinymce.init({
+    selector:'textarea#post_body',
+    plugins: "codesample image media link code",
+    toolbar: "undo redo | styleselect | bold italic link | codesample image media | code"
+});
 })
