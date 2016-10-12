581f68cfed970e299d448dc0f65b75e74a0005f9
Frank Dai <yongjindai09@gmail.com>
2016-10-12 02:58:01 +0000

    Fix for routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,11 @@ Rails.application.routes.draw do
     resources :posts
   end
 
-  scope '/blog' do
-    get 'about'     => 'blog/pages#about', as: :about
-    get 'contact'   => 'blog/pages#contact', as: :contact
-    get 'posts'     => 'blog/posts#index', as: :posts
-    get 'posts/:id' => 'blog/posts#show', as: :post
+  scope module: 'blog' do
+    get 'about'     => 'pages#about', as: :about
+    get 'contact'   => 'pages#contact', as: :contact
+    get 'posts'     => 'posts#index', as: :posts
+    get 'posts/:id' => 'posts#show', as: :post
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
